0d72ad1adac1c122689b24580421e7f4bdec217f
Tavita Vasquez <tvasquez@everis.com>
2020-07-09 16:21:12 -0500

    testt

---
+++ com.holamundo/src/test/java/com/holamundo/ws/rest/com/holamundo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holamundo.ws.rest.com.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
